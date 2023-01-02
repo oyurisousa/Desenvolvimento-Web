f235b6a643e8f04bf01ad75f513c98e4b51a4969
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-02 20:11:12 -0300

    condições-php

---
+++ 5.PHP-basico/9.condicoes/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $var1 = 15;
+    $var2 = '10 + 5';
+    //echo $var1;
+    //echo $var2;
+    if($var1 == $var2){
+        echo 'verdade';
+    }else if($var1 != $var2){
+        echo 'verdade na condição else if';
+    }
+    else{
+        echo 'mentira';
+    }
+?>
