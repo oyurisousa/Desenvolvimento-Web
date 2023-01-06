36b5406a6d78e22b9a7f67859e66ef7d518e8e02
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-06 08:26:16 -0300

    operadores mat e log

---
+++ 5.PHP-basico/11.operadores-mat-log/index.php
@@ -0,0 +1,36 @@
+<?php
+    $numero = 10;
+    $numero2 = 11;
+
+    /*
+    //operadores mateméticos
+    if($numero < $numero2){
+        //execute tal ação
+    }
+
+    if($numero <= $numero2){
+        echo "execute <="
+    }
+
+    if($numero > $numero2){
+        //execute tal ação
+    }
+
+    if($numero >= $numero2){
+        echo "execute <="
+    }
+    */
+    $var1 = "valor1";
+    $var2 = "valor2";
+    $var3 = "valor2";
+    //operador lógico 'e' - &&
+    if($var1 == $var2 && $var2 == $var3){
+        echo "verdade";
+    }
+
+    //operador lógico 'ou' - ||-
+    if($var1 == $var2 || $var2 == $var3){
+        echo "verdade";
+    }
+
+?>
