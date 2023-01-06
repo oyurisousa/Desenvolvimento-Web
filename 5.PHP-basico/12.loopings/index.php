5cdfafb2044fc0e3f8afc284484b58e0239d1777
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-06 08:38:37 -0300

    loopings

---
+++ 5.PHP-basico/12.loopings/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+    //for - para...faça
+    for($x = 0;$x<10;$x++){
+        echo "olá mundo";
+        echo '<br>';
+    }
+    
+    /*//while - enquanto ... faça
+    $x = 0;
+    while ($x < 10){
+        echo "olá mundo";
+        echo '<br>';
+        $x++;
+    }
+    */
+    /*// do while, faça ...até
+    do {
+        echo "olá mundo";
+        echo '<br>';
+        $a++;
+    } while ($a < 10);
+    */
+?>
