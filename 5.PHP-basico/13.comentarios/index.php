b3c44ad2f4a7e35709810071cd219640e5f688f7
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-07 13:28:29 -0300

    comentários

---
+++ 5.PHP-basico/13.comentarios/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    //olá, isso é um comentário, caracterizado pelo "//"
+
+    /*
+        olá sou um 
+        comentário de 
+        muitas linhas ao contrario do "//" 
+    */
+
+    # outra for forma menos usual de comentar, de uma linha tbm
+?>
