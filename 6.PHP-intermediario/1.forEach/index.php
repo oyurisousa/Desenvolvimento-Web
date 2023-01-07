4c4ad6007d604fcf325f6c60949e75a9a01c660d
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-07 13:47:45 -0300

    foreach

---
+++ 6.PHP-intermediario/1.forEach/index.php
@@ -0,0 +1,15 @@
+<?php
+    $arr = ['yuri', 'sousa','gomes','paulo'];
+    foreach ($arr as $key => $value) {
+        echo $key.' '.$value;
+        echo "<hr/>";
+    }
+
+    /*
+    $total = count($arr);
+    for($i = 0;$i < $total;$i++){
+        echo $arr[$i];
+        echo '<hr/>';
+    }
+    */
+?>
