51b0d76c917caea10ab9437302a6724e03852939
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-09 19:56:48 -0300

    die_sleep

---
+++ 6.PHP-intermediario/3.die_sleep/index.php
@@ -0,0 +1,14 @@
+<?php 
+    //durma o código por x segundos - sleep
+    /*sleep(10);
+    echo "voltei._.";
+    */
+    $nome  = 'yuri';
+    if($nome == 'joa'){
+        echo "tudo ok!";
+    }else{                 // servidor para completamente -die
+        die("morreu");
+    }
+    echo 'yueisids';
+
+?>
