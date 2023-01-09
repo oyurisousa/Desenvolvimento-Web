bd0a836cc23775db932b6385b6dd35109dd268fa
Carlos yuri <yurisousaenfer@gmail.com>
2023-01-09 19:57:38 -0300

    funções

---
+++ 6.PHP-intermediario/4.funcoes/index.php
@@ -0,0 +1,21 @@
+<?php 
+    
+    function Teste($nome,$idade){
+        echo "<h2>Seu nome é: $nome , você tem $idade anos</h2>";
+    }
+    Teste('yuri',18);
+    
+
+    /*function calculator($num1 = 10,$num2=5){
+        echo $num1 +$num2;
+    }
+    calculator();
+    */
+
+    /*
+    function verdade(){
+        return 'yuri';
+    }
+    echo verdade();
+    */
+?>
